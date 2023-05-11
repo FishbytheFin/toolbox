aa08b230f04f7faa124b65813127ac3b3bc38d1d
FishByTheFin <zxfyer84derp@gmail.com>
2023-05-11 14:33:46 -0700

    dgsgsdsf

--- source/main.c
+++ source/main.c
@@ -21,8 +21,47 @@ static C2D_SpriteSheet spriteSheet;
 static Sprite sprites[4];
 
 static void init() {
+	size_t imgCount = C2D_SpriteSheetCount(spriteSheet);
+	srand(time(NULL));
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		Sprite* sprite = &sprites[i];
+
+		C2D_SpriteFromSheet(&sprite->spr, spriteSheet, rand() % imgCount);
+		C2D_SpriteSetCenter(&sprite->spr, 0.5f, 0.5f);
+		C2D_SpriteSetPos(&sprite->spr, rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT);
+		C2D_SpriteSetRotation(&sprite->spr, C2D_Angle(rand()/(float)RAND_MAX));
+
+		sprite->dx = 2.0f;
+		sprite->dy = 3.0f;
+
+
+	}
+	
 
 }
+
+static void moveBoys() {
+	for (size_t i = 0; i < 4; i++)
+	{
+		Sprite* sprite = &sprites[i];
+
+		C2D_SpriteMove(&sprite->spr, sprite->dx, sprite->dy);
+		C2D_SpriteRotateDegrees(&sprite->spr, 1.0f);
+
+		// Check for collision with the screen boundaries
+		if ((sprite->spr.params.pos.x < sprite->spr.params.pos.w / 2.0f && sprite->dx < 0.0f) ||
+			(sprite->spr.params.pos.x > (SCREEN_WIDTH-(sprite->spr.params.pos.w / 2.0f)) && sprite->dx > 0.0f))
+			sprite->dx = -sprite->dx;
+
+		if ((sprite->spr.params.pos.y < sprite->spr.params.pos.h / 2.0f && sprite->dy < 0.0f) ||
+			(sprite->spr.params.pos.y > (SCREEN_HEIGHT-(sprite->spr.params.pos.h / 2.0f)) && sprite->dy > 0.0f))
+			sprite->dy = -sprite->dy;
+	}
+
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -38,19 +77,39 @@ int main(int argc, char *argv[])
 	// Create screens
 	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
+	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
+	if (!spriteSheet) svcBreak(USERBREAK_PANIC);
+
+	//Sandwhich
+	init();
+	
+	
 	printf("Hello, world!\n");
+	printf("I am sandwhich\n");
 
 	// Main loop
 	while (aptMainLoop())
 	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
 		hidScanInput();
 
 		// Your code goes here
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
+
+		moveBoys();
+
+		//Render
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C2D_TargetClear(top, C2D_Color32f(0.0f, 0.0f, 0.0f, 1.0f));
+		C2D_SceneBegin(top);
+
+		//Draw sprites
+		for (size_t i = 0; i < 4; i ++)
+			C2D_DrawSprite(&sprites[i].spr);
+
+		C2D_DrawRectangle(0.0f, 0.0f, 0.0f, 20.0f, 90.0f, 255.0f, 0.0f, 0.0f, 1.0f);
+		C3D_FrameEnd(0);
 	}
 
 
