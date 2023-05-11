6761727e8f66440076e5cb6de9d523f58875c195
FishByTheFin <zxfyer84derp@gmail.com>
2023-05-11 14:33:14 -0700

    added graphics

--- source/main.c
+++ source/main.c
@@ -2,11 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <3ds.h>
+#include <citro2d.h>
+#include <time.h>
+
+
+
+#define SCREEN_WIDTH  400
+#define SCREEN_HEIGHT 240
+
+
+typedef struct
+{
+	C2D_Sprite spr;
+	float dx, dy; // velocity
+} Sprite;
+
+static C2D_SpriteSheet spriteSheet;
+static Sprite sprites[4];
+
+static void init() {
+
+}
 
 int main(int argc, char *argv[])
 {
+	//Init
+	romfsInit();
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
+
+	consoleInit(GFX_BOTTOM, NULL); //Console on lower screen
+
+	// Create screens
+	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
 	printf("Hello, world!\n");
 
@@ -23,6 +53,13 @@ int main(int argc, char *argv[])
 			break; // break in order to return to hbmenu
 	}
 
+
+	// Delete graphics
+	C2D_SpriteSheetFree(spriteSheet);
+
+	C2D_Fini();
+	C3D_Fini();
 	gfxExit();
+	romfsExit();
 	return 0;
 }
