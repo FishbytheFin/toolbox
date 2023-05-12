483842942c5f3585cb1d48f1806021daf11344b2
BuildTools <unconfigured@null.spigotmc.org>
2023-05-11 21:22:28 -0700

    fixed graphics and compiled

--- source/main.c
+++ source/main.c
@@ -1,15 +1,13 @@
+#include <citro2d.h>
+
+#include <assert.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <3ds.h>
-#include <citro2d.h>
 #include <time.h>
 
-
-
-#define SCREEN_WIDTH  400
+#define SCREEN_WIDTH 400
 #define SCREEN_HEIGHT 240
-
 
 typedef struct
 {
@@ -20,70 +18,66 @@ typedef struct
 static C2D_SpriteSheet spriteSheet;
 static Sprite sprites[4];
 
-static void init() {
+static void init()
+{
 	size_t imgCount = C2D_SpriteSheetCount(spriteSheet);
 	srand(time(NULL));
 
 	for (size_t i = 0; i < 4; i++)
 	{
-		Sprite* sprite = &sprites[i];
+		Sprite *sprite = &sprites[i];
 
 		C2D_SpriteFromSheet(&sprite->spr, spriteSheet, rand() % imgCount);
 		C2D_SpriteSetCenter(&sprite->spr, 0.5f, 0.5f);
 		C2D_SpriteSetPos(&sprite->spr, rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT);
-		C2D_SpriteSetRotation(&sprite->spr, C2D_Angle(rand()/(float)RAND_MAX));
+		C2D_SpriteSetRotation(&sprite->spr, C3D_Angle(rand() / (float)RAND_MAX));
 
 		sprite->dx = 2.0f;
 		sprite->dy = 3.0f;
-
-
 	}
-	
-
 }
 
-static void moveBoys() {
+static void moveBoys()
+{
 	for (size_t i = 0; i < 4; i++)
 	{
-		Sprite* sprite = &sprites[i];
+		Sprite *sprite = &sprites[i];
 
 		C2D_SpriteMove(&sprite->spr, sprite->dx, sprite->dy);
 		C2D_SpriteRotateDegrees(&sprite->spr, 1.0f);
 
 		// Check for collision with the screen boundaries
 		if ((sprite->spr.params.pos.x < sprite->spr.params.pos.w / 2.0f && sprite->dx < 0.0f) ||
-			(sprite->spr.params.pos.x > (SCREEN_WIDTH-(sprite->spr.params.pos.w / 2.0f)) && sprite->dx > 0.0f))
+			(sprite->spr.params.pos.x > (SCREEN_WIDTH - (sprite->spr.params.pos.w / 2.0f)) && sprite->dx > 0.0f))
 			sprite->dx = -sprite->dx;
 
 		if ((sprite->spr.params.pos.y < sprite->spr.params.pos.h / 2.0f && sprite->dy < 0.0f) ||
-			(sprite->spr.params.pos.y > (SCREEN_HEIGHT-(sprite->spr.params.pos.h / 2.0f)) && sprite->dy > 0.0f))
+			(sprite->spr.params.pos.y > (SCREEN_HEIGHT - (sprite->spr.params.pos.h / 2.0f)) && sprite->dy > 0.0f))
 			sprite->dy = -sprite->dy;
 	}
-
 }
-
 
 int main(int argc, char *argv[])
 {
-	//Init
+	// Init
 	romfsInit();
 	gfxInitDefault();
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 	C2D_Prepare();
 
-	consoleInit(GFX_BOTTOM, NULL); //Console on lower screen
+	consoleInit(GFX_BOTTOM, NULL); // Console on lower screen
 
 	// Create screens
-	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+	C3D_RenderTarget *top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
 	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
-	if (!spriteSheet) svcBreak(USERBREAK_PANIC);
+	if (!spriteSheet)
+		svcBreak(USERBREAK_PANIC);
 
-	//Sandwhich
+	// Sandwhich
 	init();
-	
-	
+
 	printf("Hello, world!\n");
 	printf("I am sandwhich\n");
 
@@ -99,19 +93,18 @@ int main(int argc, char *argv[])
 
 		moveBoys();
 
-		//Render
+		// Render
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, C2D_Color32f(0.0f, 0.0f, 0.0f, 1.0f));
 		C2D_SceneBegin(top);
 
-		//Draw sprites
-		for (size_t i = 0; i < 4; i ++)
+		// Draw sprites
+		for (size_t i = 0; i < 4; i++)
 			C2D_DrawSprite(&sprites[i].spr);
 
-		C2D_DrawRectangle(0.0f, 0.0f, 0.0f, 20.0f, 90.0f, 255.0f, 0.0f, 0.0f, 1.0f);
+		// C2D_DrawRectangle(0.0f, 0.0f, 0.0f, 20.0f, 90.0f, 255.0f, 0.0f, 0.0f, 1.0f);
 		C3D_FrameEnd(0);
 	}
-
 
 	// Delete graphics
 	C2D_SpriteSheetFree(spriteSheet);
